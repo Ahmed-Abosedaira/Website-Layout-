b6af0536967596ecb946488964f120829e5d3164
Ahmed-Abosedaira <81562604+Ahmed-Abosedaira@users.noreply.github.com>
2021-07-03 10:40:25 +0200

    Add files via upload

---
+++ functionality.js
@@ -0,0 +1,9 @@
+$("#show").click(function(){
+     $("#nav-bar").show(200);
+    });
+$("#hide").click(function(){
+     $("#nav-bar").hide(200);
+    });
+$("li").click(function(){
+    $("#nav-bar").hide(200);
+  });
